abe6401fab6b126d16ef9fdd7682e77349104502
Michael <meldridge@capitoltrack.com>
2016-07-05 07:01:04 -0700

    Create Repo

---
+++ CapitolTrackMobile/Models/ReportList.cs
@@ -0,0 +1,13 @@
+﻿namespace CapitolTrackMobile.Models
+{
+	public class ReportList
+	{
+		public string ReportCategory { get; set; }
+		public string ReportName { get; set; }
+		public string ReportDescription { get; set; }
+		public string ReportLastUpdateDate { get; set; }
+
+		public string NameSort => ReportCategory[0].ToString();
+	}
+}
+
---
+++ CapitolTrackMobile/Views/ReportsPage.xaml.cs
@@ -0,0 +1,28 @@
+﻿using Xamarin.Forms;
+using Reports.ViewModels;
+using CapitolTrackMobile.Models;
+using System.Collections.Generic;
+using System.Collections;
+using System.Collections.ObjectModel;
+using System.Linq;
+
+namespace CapitolTrackMobile.Views
+{
+	public partial class ReportsPage : ContentPage
+	{
+		public ReportsPage()
+		{
+			BindingContext = new ReportsViewModel();
+		}
+
+		void Handle_ItemTapped(object sender, ItemTappedEventArgs e)
+		=> ((ListView)sender).SelectedItem = null;
+
+		void Handle_ItemSelected(object sender, SelectedItemChangedEventArgs e)
+		{
+			var report = ((ListView)sender).SelectedItem as ReportsPage;
+			if (report == null)
+				return;
+		}
+	}
+}
