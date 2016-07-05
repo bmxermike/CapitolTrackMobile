fab53e6c43930594cc6dea4f293eccec2f176661
Michael <meldridge@capitoltrack.com>
2016-07-05 09:00:13 -0700

    Remove unnesscary code from SortableLIst

--- CapitolTrackMobile/Views/ReportsPage.xaml.cs
+++ CapitolTrackMobile/Views/ReportsPage.xaml.cs
@@ -1,28 +1,25 @@
 ﻿using Xamarin.Forms;
-using Reports.ViewModels;
-using CapitolTrackMobile.Models;
 using System.Collections.Generic;
 using System.Collections;
-using System.Collections.ObjectModel;
-using System.Linq;
 
-namespace CapitolTrackMobile.Views
+namespace CapitolTrackMobile
 {
 	public partial class ReportsPage : ContentPage
 	{
 		public ReportsPage()
 		{
-			BindingContext = new ReportsViewModel();
-		}
+			InitializeComponent();
+			listView.ItemsSource = new List<ReportCategoryList>(){
+				new ReportCategoryList() { ReportCategoryTitle = "Title Here"},
+			};
 
-		void Handle_ItemTapped(object sender, ItemTappedEventArgs e)
-		=> ((ListView)sender).SelectedItem = null;
-
-		void Handle_ItemSelected(object sender, SelectedItemChangedEventArgs e)
-		{
-			var report = ((ListView)sender).SelectedItem as ReportsPage;
-			if (report == null)
-				return;
 		}
 	}
+
+	public class ReportCategoryList
+	{
+		public string ReportCategoryTitle { get; set; }
+
+	}
 }
+
