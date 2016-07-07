9bb132665c34704c7f6a99577dc55e8e49c9447a
Michael <meldridge@capitoltrack.com>
2016-07-07 11:00:09 -0700

    Edit sample report data.

--- CapitolTrackMobile/Views/ReportCategoryPage.xaml.cs
+++ CapitolTrackMobile/Views/ReportCategoryPage.xaml.cs
@@ -16,15 +16,20 @@ namespace CapitolTrackMobile
 			reports = new ObservableCollection<ReportListViewModel>();
 			InitializeComponent();
 			reportList.ItemsSource = reports;
-			reports.Add (new ReportListViewModel { ReportName = "All Bills", ReportDescricption = "Descricption will go here", ReportLastEditDate = "MM/DD/YYYY" });
-			reports.Add(new ReportListViewModel { ReportName = "All Keyword Search Report", ReportDescricption = "Descricption", ReportLastEditDate = "MM/DD/YYYY" });
-			reports.Add(new ReportListViewModel { ReportName = "Daily Report", ReportDescricption = "Descricption", ReportLastEditDate = "MM/DD/YYYY" });
-			reports.Add(new ReportListViewModel { ReportName = "Todays New Bills", ReportDescricption = "Descricption", ReportLastEditDate = "MM/DD/YYYY" });
-			reports.Add(new ReportListViewModel { ReportName = "Untracked Water Keyword Search", ReportDescricption = "Descricption", ReportLastEditDate = "MM/DD/YYYY" });
-			reports.Add(new ReportListViewModel { ReportName = "Need Attention", ReportDescricption = "Descricption", ReportLastEditDate = "MM/DD/YYYY" });
-			reports.Add(new ReportListViewModel { ReportName = "Bill Watch List", ReportDescricption = "Descricption", ReportLastEditDate = "MM/DD/YYYY" });
+			reports.Add(new ReportListViewModel { ReportName = "All Bills", ReportDescricption = "Descricption will go here", ReportLastEditDate = "MM/DD/YYYY" });
+			reports.Add(new ReportListViewModel { ReportName = "All Keyword Search Report", ReportDescricption = "Short One", ReportLastEditDate = "MM/DD/YYYY" });
+			reports.Add(new ReportListViewModel { ReportName = "Daily Report", ReportDescricption = "This is a long descricption we'll see what happens when one is really long.", ReportLastEditDate = "MM/DD/YYYY" });
+			reports.Add(new ReportListViewModel { ReportName = "Todays New Bills", ReportDescricption = "Descricption will go here", ReportLastEditDate = "MM/DD/YYYY" });
+			reports.Add(new ReportListViewModel { ReportName = "Untracked Water Keyword Search", ReportDescricption = "Another one", ReportLastEditDate = "MM/DD/YYYY" });
+			reports.Add(new ReportListViewModel { ReportName = "Need Attention", ReportDescricption = "Stuff Here", ReportLastEditDate = "MM/DD/YYYY" });
+			reports.Add(new ReportListViewModel { ReportName = "Bill Watch List", ReportDescricption = "Descricption will go here", ReportLastEditDate = "MM/DD/YYYY" });
+			reports.Add(new ReportListViewModel { ReportName = "All Bills", ReportDescricption = "Descricption will go here", ReportLastEditDate = "MM/DD/YYYY" });
+			reports.Add(new ReportListViewModel { ReportName = "All Keyword Search Report", ReportDescricption = "Short One", ReportLastEditDate = "MM/DD/YYYY" });
+			reports.Add(new ReportListViewModel { ReportName = "Daily Report", ReportDescricption = "This is a long descricption we'll see what happens when one is really long.", ReportLastEditDate = "MM/DD/YYYY" });
+			reports.Add(new ReportListViewModel { ReportName = "Todays New Bills", ReportDescricption = "Descricption will go here", ReportLastEditDate = "MM/DD/YYYY" });
+			reports.Add(new ReportListViewModel { ReportName = "Untracked Water Keyword Search", ReportDescricption = "Another one", ReportLastEditDate = "MM/DD/YYYY" });
+			reports.Add(new ReportListViewModel { ReportName = "Need Attention", ReportDescricption = "Stuff Here", ReportLastEditDate = "MM/DD/YYYY" });
+			reports.Add(new ReportListViewModel { ReportName = "Bill Watch List", ReportDescricption = "Descricption will go here", ReportLastEditDate = "MM/DD/YYYY" });
 		}
 	}
 }
-
-	
